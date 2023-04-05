0db375adab4854e546fb37e1588f2c63056ab90d
BlebDaPleb <akazhar123@gmail.com>
2023-04-05 15:26:06 -0400

    Initial commit

---
+++ src/main/java/blebdapleb/arsenic/arsenic/Arsenic.java
@@ -0,0 +1,10 @@
+package blebdapleb.arsenic.arsenic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Arsenic implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
